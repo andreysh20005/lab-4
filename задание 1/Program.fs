ce5fb36f09e367c0e72f0b0dba7754f946c0a337
andreysh20005 <andrejsemakin838@gmail.com>
2026-03-20 01:13:32 +0500

    Update Program.fs

--- задание 1/Program.fs
+++ задание 1/Program.fs
@@ -1,95 +1,69 @@
-﻿//Сумма элементов дерева
 open System
 open System.Text
 
 type 't btree =
-    Node of 't * 't btree * 't btree
+    | Node of 't * 't btree * 't btree
     | Nil
 
 
 
 let infix root left right = 
-    (
-         root();left(); right()
-    )
+    (root(); left(); right())
 
 let iterh trav f t =
     let rec tr t h =
         match t with
-        Node (x,L,R) -> trav
-                            (fun () -> (f x h)) 
-                            (fun () -> tr L (h+1))
-                            (fun () -> tr R (h+1));
+        | Node (x, L, R) -> 
+            trav
+                (fun () -> f x h)      
+                (fun () -> tr L (h+1)) 
+                (fun () -> tr R (h+1)) 
         | Nil -> ()
     tr t 0
 
-
-
-let spaces n =
-    List.fold (fun s _ -> s+".") "" [0..n]
+let spaces n = List.fold (fun s _ -> s + ".") "" [0..n]
 
 let printTree T =
-    iterh infix (fun x h -> printfn "%s%A" (spaces h)x) T
-
+    iterh infix (fun x h -> printfn "%s%A" (spaces h) x) T
 
 
 let rec insert t x =
-        match t with
-            Nil -> Node(x,Nil,Nil)
-            | Node(z,L,R) -> 
-                if x<z then Node(z,insert L x ,R)
-                else Node(z,L,insert R x)
+    match t with
+    | Nil -> Node(x, Nil, Nil)
+    | Node(z, L, R) ->
+        if x < z then Node(z, insert L x, R)
+        else Node(z, L, insert R x)
 
 let treeFold f st tree =   
-        let rec ob d k = 
-            match d with
-            | Node (t,l,r) -> ob r (f (ob l k) t)
-            | Nil -> k
-        ob tree st
-
-
-let rec mapTree f tree =
-    match tree with
-    | Nil -> Nil
-    | Node(x, left, right) -> Node(f x, mapTree f left, mapTree f right)
+    let rec ob d k = 
+        match d with
+        | Node (t, l, r) -> ob r (f (ob l k) t)
+        | Nil -> k
+    ob tree st
 
 
 
-let randStr () =
-    let r2 = new Random()
-    let length = int(r2.Next(1, 20))
-
-    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".ToCharArray()
-    let buffer = Random.Shared.GetItems(chars, length)
-    new string(buffer)
-    
-let incrementLastChar (str:string) =
-    if str.Length > 0 then
-        str.[0..str.Length-2] + (char (int str.[str.Length-1] + 1)).ToString()
-    else
-        ""
-
-
+let randFloat () =
+    let r = Random()
+    r.NextDouble() * 200.0 - 100.0
 
 
 
 [<EntryPoint>]
 let main _ =
-    
-    let A =
-        [            
-            let r1 = new Random()
-            let n = int(r1.Next(5, 15))
-            for i in 1..n do
-                yield randStr ()
-        ]
-    printfn "Исходный список %A\n\n" A
+
+    let rnd = Random()
+    let count = rnd.Next(5, 15)
+    let A = List.init count (fun _ -> randFloat ())
+    printfn "Исходный список:\n%A\n" A
+
     let startBinTree = A |> List.fold insert Nil
-    printfn "исходное дерево:\n\n"
+    printfn "Исходное дерево:"
     printTree startBinTree
 
-    let newBinTree = treeFold (fun acc x -> insert acc (incrementLastChar x)) Nil startBinTree
-    printfn "итоговое дерево (инкрементирован последний символ строки):\n\n"
+
+    let newBinTree = treeFold (fun acc x -> insert acc (int x)) Nil startBinTree
+    printfn "\nНовое дерево (целые части исходных чисел)\n:"
     printTree newBinTree
 
     0
